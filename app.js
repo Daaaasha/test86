e5c37e24772a45468d17efe512673694c19912ac
Daaaasha <88614769+Daaaasha@users.noreply.github.com>
2021-08-09 16:27:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+scores = 0
+a = prompt("Выберете язык разметки: a)HTML b) CSS c)JS");
+if (a == "a") {
+scores +=2;
+}
+
+a = prompt("Выбирете число: a)1 b) 7 c) 6");
+if  (c == "c"){
+  scores +=2;
+}
+
+
+
+alert(` ${scores} - результат.`)
